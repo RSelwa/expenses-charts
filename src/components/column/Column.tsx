eadb24df647e9b2955df05e888a95394d73d0fc3
selwa <raphsp@orange.fr>
2022-05-27 15:50:26 +0200

    fail to load scss variables

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -3,15 +3,12 @@ const Column = (props: any) => {
   return (
     <div className="column">
       <div
-        // className={Math.floor(props.amount).toString()}
         className="amount"
         title={props.amount}
         style={{
           height: `${Math.ceil((props.amount * 100) / props.max) * 0.5}%`,
         }}
-        // style={{ height: `${props.amount * 6}px` }}
       />
-      {/* {props.amount} */}
       <div className="day">{props.day}</div>
       <div className="day">{Math.ceil((props.amount * 100) / props.max)}%</div>
     </div>
