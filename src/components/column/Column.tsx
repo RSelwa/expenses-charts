f9985090a1b7484bffb241d2e5de85a8b362b831
selwa <raphsp@orange.fr>
2022-05-28 12:34:58 +0200

    css variables as ts variables

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,12 +1,36 @@
 import "./column.css";
+import "./../../css/variables";
+import cssVariables from "./../../css/variables";
+import { useState } from "react";
+
 const Column = (props: any) => {
+  const [bgColor, setBgColor] = useState(
+    cssVariables.filter((e) => {
+      return e.name == "red";
+    })[0].value
+  );
   return (
     <div className="column">
       <div
         className="amount"
-        title={props.amount}
+        title={props.amount + "€"}
+        onMouseEnter={() => {
+          setBgColor(
+            cssVariables.filter((e) => {
+              return e.name == "cyan";
+            })[0].value
+          );
+        }}
+        onMouseLeave={() => {
+          setBgColor(
+            cssVariables.filter((e) => {
+              return e.name == "red";
+            })[0].value
+          );
+        }}
         style={{
           height: `${Math.ceil((props.amount * 100) / props.max) * 0.5}%`,
+          backgroundColor: bgColor,
         }}
       />
       <div className="day">{props.day}</div>
